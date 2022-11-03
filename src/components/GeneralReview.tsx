a2af470da58141c13f6750acfc9fc5741ff35949
Dan Benson <dbenson3091@gmail.com>
2022-11-02 23:37:20 -0500

    created styling for form review

--- src/components/GeneralReview.tsx
+++ src/components/GeneralReview.tsx
@@ -1,23 +1,52 @@
 import { CreateRecipe } from '../interface';
 import { Typography, Grid } from '@mui/material';
+import React from 'react';
 
 function GeneralReview({ formValues }: { formValues: CreateRecipe}) {
-    const { recipeName, prepTime, cookingTime, mealType } = formValues;
-
-    console.log(formValues)
+    const { 
+        recipeName, 
+        prepTime, 
+        cookingTime, 
+        mealType, 
+        ingredientList 
+    } = formValues;
 
     return (
-        <Grid item xs={12} sm={6}>
-          <Typography variant="h6" gutterBottom>
-            General Info
-          </Typography>
-          <Typography gutterBottom>Name: {recipeName}</Typography>
-          <Typography gutterBottom>Prep Time: {prepTime || '0'}</Typography>
-          <Typography gutterBottom>Cooking Time: {cookingTime}</Typography>
-          <Typography gutterBottom>
-            Meal Type: {mealType || 'Not Specified'}
-          </Typography>
-        </Grid>
+        <>
+            <Grid item xs={12} sm={6}>
+                <Typography variant="h6" gutterBottom>
+                    General Info
+                </Typography>
+                <Typography gutterBottom>Name: {recipeName}</Typography>
+                <Typography gutterBottom>
+                    Prep Time: {prepTime || '0'}
+                </Typography>
+                <Typography gutterBottom>
+                    Cooking Time: {cookingTime}
+                </Typography>
+                <Typography gutterBottom>
+                    Meal Type: {mealType || 'Not Specified'}
+                </Typography>
+            </Grid>
+
+            <Grid item container direction="column" xs={12} sm={6}>
+                <Typography variant="h6" gutterBottom>
+                    Ingredient List
+                </Typography>
+
+                <Grid container>
+                    {ingredientList?.map(({amount, ingredient, measurement}, idx) => (
+                        <React.Fragment key={idx}>
+                            <Grid item xs={6}>
+                                <Typography gutterBottom>
+                                    {`${amount} ${measurement || '' } ${ingredient}`}
+                                </Typography>
+                            </Grid>
+                        </React.Fragment>
+                    ))}
+                </Grid>
+            </Grid>
+        </>
       );
 };
 
