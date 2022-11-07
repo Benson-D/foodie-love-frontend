1360e352b4180c8b25beb09e70ce1784b1d03025
Dan Benson <dbenson3091@gmail.com>
2022-11-06 23:19:15 -0600

    updated home page, removed css styles

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,18 @@
-import {Box, Typography } from '@mui/material';
-import '../styles/HomePage.css';
+import { Typography } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import homeImage from '../img/foodie-background.jpg';
 
-const homePageDesign = {
-    py: 7, 
-    pl: 3, 
-    color: 'white'
-};
+const HomeLayout = styled('section')({
+    backgroundImage: `url(${homeImage})`,
+    backgroundPosition: 'bottom left',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    color: '#fff',
+    height: '40vh',
+    paddingTop: 56,
+    paddingBottom: 56,
+    paddingLeft: 26,
+})
 
 /**
  * Initial page when a user is signed in or not
@@ -17,10 +24,10 @@ const homePageDesign = {
  */
 function HomePage() {
     return (
-        <Box sx={homePageDesign} className="foodieHome" component="section">
+        <HomeLayout>
             <Typography variant="h3">Welcome To Foodie Love!</Typography>
             <p>A place to keep your recipes in check</p>
-        </Box>
+        </HomeLayout>
     );
 }
 
