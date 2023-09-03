b658425d6881b90fb61a68f6cfb5d4bff081da89
Dan Benson <dbenson3091@gmail.com>
2023-09-03 15:10:10 -0500

    created a loader spinner

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,16 @@
+import { Box, CircularProgress } from '@mui/material';
+
+/**
+ * Main Load Spinner for page rendering 
+ * Props: none
+ * State: none
+ */
+function Loader() {
+  return (
+    <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: '5rem' }}>
+        <CircularProgress />
+    </Box>
+  )
+}
+
+export default Loader ;
