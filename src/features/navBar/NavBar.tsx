4ab84c8cdcf25c71c9baf57ff8bc84b205028468
Dan Benson <dbenson3091@gmail.com>
2023-10-10 21:21:04 -0500

    updated nav bar to be responsive, and utilize avatar image for when user has signed in

--- src/features/navBar/NavBar.tsx
+++ src/features/navBar/NavBar.tsx
@@ -1,58 +1,118 @@
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import MenuIcon from "@mui/icons-material/Menu";
-import HomeIcon from "@mui/icons-material/Home";
 import RestaurantMenuIcon from "@mui/icons-material/RestaurantMenu";
+import StorefrontIcon from "@mui/icons-material/Storefront";
 import {
   Box,
-  CssBaseline,
+  Container,
   AppBar,
   Toolbar,
+  Tooltip,
   Typography,
   Button,
   IconButton,
+  Avatar,
 } from "@mui/material";
 import useToggle from "../../hooks/useToggle";
 import SideModal from "../../components/SideModal";
 import ListItems from "./ListItems";
+import { LockOpen, Lock } from "@mui/icons-material";
 
 const navItems = [
   {
-    title: "Home",
-    link: "/",
-    icon: <HomeIcon />,
+    title: "Login",
+    link: "/login",
+    icon: <LockOpen />,
   },
+];
+
+const navItemsAuth = [
   {
     title: "Recipes",
     link: "/recipes",
     icon: <RestaurantMenuIcon />,
   },
+  {
+    title: "Logout",
+    link: "/",
+    icon: <Lock />,
+  },
 ];
 
-/**
- * Main nav bar for Foodie Recipe page,
- * responsive and can open close menu bar in mobile display
- *
- * Props: none
- * State: none
- *
- * App -> Nav
- */
-function NavBar() {
+function MobileNavBar({
+  navItems,
+}: {
+  navItems: { title: string; link: string; icon: JSX.Element }[];
+}) {
   const [value, toggleValue] = useToggle();
+
   return (
-    <Box sx={{ display: "flex" }}>
-      <CssBaseline />
-      <AppBar
-        component="nav"
-        position="static"
-        sx={{ backgroundColor: "#04b597" }}
+    <>
+      <StorefrontIcon sx={{ display: { xs: "flex", sm: "none" }, mr: 1 }} />
+      <Typography
+        variant="h5"
+        noWrap
+        sx={{
+          display: { xs: "flex", sm: "none" },
+          fontFamily: "monospace",
+          fontWeight: 700,
+          flexGrow: 1,
+          letterSpacing: ".15rem",
+          textDecoration: "none",
+          textAlign: "center",
+        }}
       >
-        <Toolbar>
-          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+        Foodie Love
+      </Typography>
+      <IconButton
+        color="inherit"
+        aria-label="open drawer"
+        onClick={() => toggleValue()}
+        sx={{ display: { sm: "none" }, ml: "auto" }}
+      >
+        <MenuIcon />
+      </IconButton>
+      <SideModal modalOpen={value} handleToggle={() => toggleValue()}>
+        <ListItems handleToggle={() => toggleValue()} listItems={navItems} />
+      </SideModal>
+    </>
+  );
+}
+
+function NavBar() {
+  const user = useSelector((state: any) => state.app.authUser as any) as any;
+
+  const navItemsDisplayed = user !== null ? navItemsAuth : navItems;
+
+  console.log(user, "valid user");
+
+  return (
+    <AppBar position="static" sx={{ backgroundColor: "#04b597" }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <StorefrontIcon sx={{ display: { xs: "none", sm: "flex" }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            sx={{
+              display: { xs: "none", sm: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".15rem",
+              textDecoration: "none",
+            }}
+          >
             Foodie Love
           </Typography>
-          <Box sx={{ display: { xs: "none", sm: "block" } }}>
-            {navItems.map((item, idx) => (
+          <Box
+            sx={{
+              flexGrow: 1,
+              justifyContent: "end",
+              display: { xs: "none", sm: "flex" },
+            }}
+          >
+            {navItemsDisplayed.map((item, idx) => (
               <Button
                 key={idx}
                 component={Link}
@@ -63,21 +123,22 @@ function NavBar() {
               </Button>
             ))}
           </Box>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={() => toggleValue()}
-            sx={{ display: { sm: "none" } }}
-          >
-            <MenuIcon />
-          </IconButton>
+          {user && (
+            <Box sx={{ display: "flex", flexGrow: { xs: 1, sm: 0 } }}>
+              <Tooltip title="Open settings">
+                <IconButton>
+                  <Avatar
+                    alt="foodie-user"
+                    src={user && user?.imageUrl ? user.imageUrl : ""}
+                  />
+                </IconButton>
+              </Tooltip>
+            </Box>
+          )}
+          <MobileNavBar navItems={navItems} />
         </Toolbar>
-      </AppBar>
-
-      <SideModal modalOpen={value} handleToggle={() => toggleValue()}>
-        <ListItems handleToggle={() => toggleValue()} listItems={navItems} />
-      </SideModal>
-    </Box>
+      </Container>
+    </AppBar>
   );
 }
 
