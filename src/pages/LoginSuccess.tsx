0b55f300df9127fd060c332042ee0e8ec5790479
Dan Benson <dbenson3091@gmail.com>
2024-01-06 18:59:07 -0600

    updated login access

--- src/pages/LoginSuccess.tsx
+++ src/pages/LoginSuccess.tsx
@@ -1,23 +1,32 @@
 import { Typography } from "@mui/material";
 import { useEffect } from "react";
-import { useGetAuthUserQuery } from "../service/authService";
-import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { setAuthUser, setToken } from "../appSlice";
+// import { useGetAuthUserQuery } from "../service/authService";
+// import { useDispatch } from "react-redux";
+// import { useNavigate } from "react-router-dom";
+// import { setAuthUser, setToken } from "../appSlice";
 
 function LoginSuccess() {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const { data } = useGetAuthUserQuery();
+  // const dispatch = useDispatch();
+  // const navigate = useNavigate();
+  // const { data, error } = useGetAuthUserQuery();
+
+  // console.log(data, "<==== data from request", error, "<==== error from request");
+
+  // useEffect(() => {
+  //   setTimeout(() => navigate("/recipes"), 500);
+
+  //   if (data) {
+  //     dispatch(setAuthUser(data?.user ?? null));
+  //     dispatch(setToken(data?.token ?? null));
+
+  //   }
+  // }, [data]);
 
   useEffect(() => {
-    if (data) {
-      dispatch(setAuthUser(data?.user ?? null));
-      dispatch(setToken(data?.token ?? null));
-
-      setTimeout(() => navigate("/recipes"), 500);
-    }
-  }, [data]);
+    setTimeout(() => {
+      window.close();
+    }, 1000);
+  }, []);
 
   return (
     <Typography sx={{ color: "black", padding: "10px" }}>
