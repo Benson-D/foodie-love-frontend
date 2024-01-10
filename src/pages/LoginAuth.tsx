dcd5bc1e46ab512b3bf2239af5d11cc35bdf7c33
Dan Benson <dbenson3091@gmail.com>
2024-01-09 21:09:56 -0600

    updated login status

--- src/pages/LoginAuth.tsx
+++ src/pages/LoginAuth.tsx
@@ -1,40 +1,10 @@
 import { Container, Paper, Grid, Box, Avatar, Typography } from "@mui/material";
 import GoogleButton from "react-google-button";
 import { LockOutlined } from "@mui/icons-material";
-import axios from "axios";
-
-const fetchAuthUser = async () => {
-  try {
-    const response = await axios.get(
-      "https://foodieloveapi.onrender.com/auth/user",
-      { withCredentials: true },
-    );
-
-    console.log(response, "validating response");
-  } catch (err) {
-    console.error(err);
-  }
-};
 
 function LoginAuth() {
   const redirectToGoggleSSO = () => {
-    //location.href = "https://foodieloveapi.onrender.com/auth/google";
-    //location.href = "http://localhost:3001/auth/google";
-    let timer: number | null | undefined = null;
-    const loginURL = "https://foodieloveapi.onrender.com/auth/google";
-
-    const newWindow = window.open(loginURL, "_blank", "width=500, height=600");
-
-    if (newWindow) {
-      timer = setInterval(() => {
-        if (newWindow.closed) {
-          console.log("Window has closed");
-          fetchAuthUser();
-
-          if (timer) clearInterval(timer);
-        }
-      }, 500);
-    }
+    location.href = "https://foodieloveapi.onrender.com/auth/google";
   };
 
   return (
--- src/pages/LoginSuccess.tsx
+++ src/pages/LoginSuccess.tsx
@@ -1,32 +1,30 @@
 import { Typography } from "@mui/material";
 import { useEffect } from "react";
-// import { useGetAuthUserQuery } from "../service/authService";
-// import { useDispatch } from "react-redux";
-// import { useNavigate } from "react-router-dom";
-// import { setAuthUser, setToken } from "../appSlice";
+import { useGetAuthUserQuery } from "../service/authService";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { setAuthUser, setToken } from "../appSlice";
 
 function LoginSuccess() {
-  // const dispatch = useDispatch();
-  // const navigate = useNavigate();
-  // const { data, error } = useGetAuthUserQuery();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { data, error } = useGetAuthUserQuery();
 
-  // console.log(data, "<==== data from request", error, "<==== error from request");
-
-  // useEffect(() => {
-  //   setTimeout(() => navigate("/recipes"), 500);
-
-  //   if (data) {
-  //     dispatch(setAuthUser(data?.user ?? null));
-  //     dispatch(setToken(data?.token ?? null));
-
-  //   }
-  // }, [data]);
+  console.log(
+    data,
+    "<==== data from request",
+    error,
+    "<==== error from request",
+  );
 
   useEffect(() => {
-    setTimeout(() => {
-      window.close();
-    }, 1000);
-  }, []);
+    setTimeout(() => navigate("/login"), 9000);
+
+    if (data) {
+      dispatch(setAuthUser(data?.user ?? null));
+      dispatch(setToken(data?.token ?? null));
+    }
+  }, [data]);
 
   return (
     <Typography sx={{ color: "black", padding: "10px" }}>
